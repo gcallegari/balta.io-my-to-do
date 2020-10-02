63648a5d630932842b94e27dea2fc57d25b7ef63
guilherme <xcallegarix.99@gmail.com>
2020-10-02 14:55:40 -0300

    models

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar';
+import TodoList from './TodoList';
+
+const App = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <TodoList></TodoList>
+        </div>
+    )
+}
+
+export default App;
